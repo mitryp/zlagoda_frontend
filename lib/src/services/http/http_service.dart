c1e92904c601479470e25c46e67209916d072a97
Dmytro Popov <thedmitryp@ukr.net>
2023-03-31 22:42:42 +0100

    Added castFunction to HttpService class

--- lib/src/services/http/http_service.dart
+++ lib/src/services/http/http_service.dart
@@ -1,11 +1,14 @@
 import '../../model/model.dart';
 import '../query_builder/query_builder.dart';
 
+typedef JsonCastFunction<M extends Model> = M Function(dynamic json);
+
 // todo @Kate
 abstract class HttpService<M extends Model> {
   final QueryBuilder queryBuilder;
+  final JsonCastFunction castFunction;
 
-  const HttpService(this.queryBuilder);
+  const HttpService({required this.queryBuilder, required this.castFunction});
 
   Future<List<M>> get();
 
