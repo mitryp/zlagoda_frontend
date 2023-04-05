c5c1a6e4f10544ae0eefcfc95b2bf5248bd8ac53
Dmytro Popov <thedmitryp@ukr.net>
2023-04-05 23:56:35 +0100

    Implemented `LoginManager` and `AuthService`
    Unstable: `AuthService` requires testing with a real server or mock
    Added `flutter_secure_storage` dependency

--- lib/src/services/middleware/response/auth_handle_middleware.dart
+++ lib/src/services/middleware/response/auth_handle_middleware.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' show Response;
 
+import '../../auth/login_manager.dart';
 import 'response_middleware.dart';
 
 /// A middleware that redirects the [Navigator] of the context to the /login endpoint, if a
 /// processed response has a status code of 401.
 ///
-class LoginRedirectMiddleware extends ResponseMiddleware {
-  const LoginRedirectMiddleware(super.context) : super(statusCodeFilter: unauthorized);
+class AuthHandleMiddleware extends ResponseMiddleware {
+  const AuthHandleMiddleware(super.context) : super(filter: unauthorized);
 
   @override
-  Response processFilteredResponse(Response res) {
+  Response processFiltered(Response r) {
     WidgetsBinding.instance.addPersistentFrameCallback((_) {
+      LoginManager.of(context).clearLoginData();
       Navigator.maybeOf(context)?.pushReplacementNamed('/login');
     });
 
-    return res;
+    return r;
   }
 }
