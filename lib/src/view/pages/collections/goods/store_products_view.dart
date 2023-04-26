cbd41905fd070122d2096e334d0883d45516c63b
Kathryn Verkhohliad <kathrine312003@gmail.com>
2023-04-26 22:10:51 +0300

    filter by upc on store products view

--- lib/src/view/pages/collections/goods/store_products_view.dart
+++ lib/src/view/pages/collections/goods/store_products_view.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../../../../model/basic_models/store_product.dart';
 import '../../../../model/joined_models/joined_store_product.dart';
+import '../../../../model/search_models/short_product.dart';
 import '../../../../services/query_builder/filter.dart';
 import '../../../../services/query_builder/sort.dart';
 import '../../../../utils/navigation.dart';
 import '../../../../utils/value_status.dart';
+import '../../../widgets/queries/connected_model_filter.dart';
 import '../../../widgets/queries/filters/chips_filter.dart';
 import '../../../widgets/queries/sort_block.dart';
 import '../../../widgets/resources/collections/collection_view.dart';
@@ -47,7 +49,15 @@ class StoreProductsSearchFilters extends CollectionSearchFilterDelegate {
 
   @override
   List<Widget> buildSearches(BuildContext context) {
-    return [];
+    return [
+      ConnectedModelFilter<String, ShortProduct>(
+        filterOption: FilterOption.upc,
+        addFilter: addFilter,
+        removeFilterByOption: removeFilter,
+        caption: 'Всі товари',
+        searchHint: 'Пошук за upc, назвою товару, виробником...',
+      ),
+    ];
   }
 
   @override
